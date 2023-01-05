28b85e14487783369183fbb2c77e68ea71daaefa
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2023-01-05 19:17:29 +0900

    Move entrypoints to a separate file

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,14 @@
-use std::{cmp, thread};
 use std::collections::{BTreeMap, VecDeque};
-use std::ptr::NonNull;
-use std::sync::{Arc, Mutex, Once};
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex, Once};
 use std::time::Duration;
+use std::{cmp, thread};
 
 use crate::ewma::EwmaEstimator;
 use crate::profiler::Profiler;
 use crate::time::*;
 
+mod entrypoint;
 mod ewma;
 mod profiler;
 #[cfg_attr(unix, path = "time/unix.rs")]
@@ -72,7 +72,7 @@ struct FrameImpl {
 }
 
 impl ContextInner {
-    fn frames_iter(&self) -> impl DoubleEndedIterator<Item=&FrameImpl> {
+    fn frames_iter(&self) -> impl DoubleEndedIterator<Item = &FrameImpl> {
         self.reference_frame.iter().chain(self.frames.values())
     }
 
@@ -80,10 +80,10 @@ impl ContextInner {
         self.reference_frame.as_ref().map(|reference_frame| {
             reference_frame.end_ts()
                 + self
-                .frames
-                .iter()
-                .map(|(_, frame)| frame.predicted_duration)
-                .sum::<u64>()
+                    .frames
+                    .iter()
+                    .map(|(_, frame)| frame.predicted_duration)
+                    .sum::<u64>()
         })
     }
 
@@ -155,11 +155,11 @@ impl ContextInner {
         const MAX_LATENCY: u64 = 200_000_000;
 
         while let Some((
-                           _,
-                           FrameImpl {
-                               writer_count: 0, ..
-                           },
-                       )) = self.frames.first_key_value()
+            _,
+            FrameImpl {
+                writer_count: 0, ..
+            },
+        )) = self.frames.first_key_value()
         {
             let (_, frame) = self.frames.pop_first().unwrap();
 
@@ -339,102 +339,4 @@ impl ImplicitContext {
         eprintln!("LFX2: Resetting implicit context: swapchain recreated");
         self.need_reset.store(true, Ordering::SeqCst);
     }
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2TimestampNow() -> Timestamp {
-    timestamp_now()
-}
-
-#[cfg(target_os = "windows")]
-#[no_mangle]
-pub unsafe extern "C" fn lfx2TimestampFromQpc(qpc: u64) -> Timestamp {
-    timestamp_from_qpc(qpc)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2SleepUntil(target: Timestamp) {
-    sleep_until(target)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2ContextCreate() -> *mut Context {
-    Arc::into_raw(Arc::new(Context::default())) as _
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2ContextAddRef(context: *mut Context) {
-    Arc::increment_strong_count(context);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2ContextRelease(context: *mut Context) {
-    Arc::decrement_strong_count(context);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2FrameCreate(
-    context: *mut Context,
-    out_timestamp: *mut Timestamp,
-) -> *mut Frame {
-    let context = Arc::from_raw(context);
-    let (frame, timestamp) = context.inner.lock().unwrap().prepare_frame(context.clone());
-    *out_timestamp = timestamp;
-    let _ = Arc::into_raw(context);
-    Arc::into_raw(frame) as _
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2FrameAddRef(frame: *mut Frame) {
-    Arc::increment_strong_count(frame);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2FrameRelease(frame: *mut Frame) {
-    Arc::decrement_strong_count(frame);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2MarkSection(
-    frame: *mut Frame,
-    section_id: SectionId,
-    mark_type: MarkType,
-    timestamp: Timestamp,
-) {
-    (*frame).mark(section_id, mark_type, timestamp);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2ImplicitContextCreate() -> *mut ImplicitContext {
-    let context = Box::new(ImplicitContext::default());
-    Box::into_raw(context)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2ImplicitContextRelease(context: *mut ImplicitContext) {
-    let _ = Box::from_raw(context);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2ImplicitContextReset(context: *mut ImplicitContext) {
-    (*context).reset();
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2FrameCreateImplicit(
-    context: *mut ImplicitContext,
-    out_timestamp: *mut Timestamp,
-) -> *mut Frame {
-    let (frame, timestamp) = (*context).enqueue();
-    *out_timestamp = timestamp;
-    Arc::into_raw(frame) as _
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn lfx2FrameDequeueImplicit(
-    context: *mut ImplicitContext,
-    critical: bool,
-) -> Option<NonNull<Frame>> {
-    let frame = (*context).dequeue(critical);
-    frame.map(|f| NonNull::new(Arc::into_raw(f) as _).unwrap())
 }
